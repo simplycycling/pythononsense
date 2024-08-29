6fb29c87c28eef486eadb292bda7e993e05d6e71
Roger Sherman <rsherman@linux.com>
2024-08-29 19:23:22 +1000

    First half of python loops

---
+++ python_basics_loops/greet_your_friends.py
@@ -0,0 +1,4 @@
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for x in friends:
+    print(f"Hello, {x}!")
---
+++ python_basics_loops/triple_greeting.py
@@ -0,0 +1,9 @@
+a = 3
+greeting = 'Aloha!'
+
+while a > 0:
+    print(greeting)
+    a -= 1
+print()
+for _ in range(3):
+    print(greeting)

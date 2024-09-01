60bc6cd8048d859355e9b1feeaaa9f162caa79fd
Roger Sherman <rsherman@linux.com>
2024-09-01 13:32:30 +1000

    launch school case statement

---
+++ python_basics_conditionals/check_the_weather_part_2.py
@@ -0,0 +1,12 @@
+weather = 'cloudy'
+
+match weather:
+    case 'sunny':
+        print('Wear sunscreen')
+    case 'rainy':
+        print('Bring umbrella')
+    case 'cloudy':
+        print('bring umbrella just in case')
+    case 'pompy':
+        print('gross')
+

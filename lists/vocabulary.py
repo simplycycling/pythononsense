1e550e52e1f169fe71737de98f1bd3dfa064261b
Roger Sherman <rsherman@linux.com>
2024-09-16 18:55:38 +1000

    iterating through nested lists

---
+++ lists/vocabulary.py
@@ -0,0 +1,9 @@
+vocabulary = [
+    ["happy", "cheerful", "merry", "glad"],
+    ["tired", "sleepy", "fatigued", "drained"],
+    ["excited", "eager", "enthused", "animated"],
+]
+
+for el in vocabulary:
+    for i in el:
+        print(i)

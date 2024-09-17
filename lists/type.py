fac885930246e410990efacde1ae8a669f6c8486
Roger Sherman <rsherman@linux.com>
2024-09-17 20:43:19 +1000

    Checking type

---
+++ lists/type.py
@@ -0,0 +1,7 @@
+# Given two variables, verify whether the values they hold are lists.
+
+some_value1 = [0, 1, 0, 0, 1]
+some_value2 = "I leave you my Kingdom, take good care of it."
+
+print(isinstance(some_value1, list))
+print(isinstance(some_value2, list))

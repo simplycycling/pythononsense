167454ec1f018939df2b6d7b516ba80187c5ebcd
Roger Sherman <rsherman@linux.com>
2024-09-18 19:49:09 +1000

    First shot at manually writing in

---
+++ lists/travel.py
@@ -0,0 +1,23 @@
+def contains(city, var_name):
+    for v in var_name:
+        if v == city:
+            return True
+
+
+destinations = [
+    "Prague",
+    "London",
+    "Sydney",
+    "Belfast",
+    "Rome",
+    "Aruba",
+    "Paris",
+    "Bora Bora",
+    "Barcelona",
+    "Rio de Janeiro",
+    "Marrakesh",
+    "New York City",
+]
+
+print(contains("Barcelona", destinations))
+print(contains("Nashville", destinations))
